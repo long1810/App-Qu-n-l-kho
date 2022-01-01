da20d2dea03936c79e9b8972f9903a6380934d38
long1810 <63438617+long1810@users.noreply.github.com>
2022-01-01 18:42:55 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,94 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import React from 'react';
+import C from './src/Component/Yugi.js';
+// import C from './src/Component/lux.js';
+
+const App: () => React$Node = () => {
+  
+  
+  return (
+    <>
+    <C></C>
+
+    </>
+  );
+};
+export default App;
+
+     {/* <NavigationContainer>
+       
+      <Stack.Navigator initialRouteName="screen1">
+
+           <Stack.Screen name="screen1" component={B_tab}></Stack.Screen>
+          <Stack.Screen name="screen2" component={Add}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>   */}
+
+// import Movie from './src/Component/Movie.js';
+// function TabClock() {
+//   return(
+//     <Clock></Clock>
+//     // <Text>Clock</Text>
+//   );
+// }
+// function TabAlarm() {
+//   return(
+//     <Alarm></Alarm>
+//     // <Text>Alarm</Text>
+//   );
+// }
+// function TabStopWatch() {
+//   return(
+//     <StopWatch></StopWatch>
+//     // <Text>StopWatch</Text>
+//   );
+// }
+// function TabTimer() {
+//   return(
+//     <Timer></Timer>
+//     // <Text>Timer</Text>
+//   );
+// }
+// import {
+//   SafeAreaView,
+//   StyleSheet,
+//   ScrollView,
+//   View,
+//   Text,
+//   StatusBar,
+// } from 'react-native';
+
+// import {
+//   Header,
+//   LearnMoreLinks,
+//   Colors,
+//   DebugInstructions,
+//   ReloadInstructions,
+// } from 'react-native/Libraries/NewAppScreen';
+
+// import Clock from './src/Component/Clock.js'
+// import Alarm from './src/Component/Alarm.js'
+// import StopWatch from './src/Component/StopWatch.js'
+// import Timer from './src/Component/Timer.js'
+
+// import {NavigationContainer} from '@react-navigation/native';
+// import { createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+// import { createStackNavigator } from '@react-navigation/stack';
+
+// import Pizaa from './src/Component/Pizza.js';
+// import Add from './src/Component/add.js';
+// import B_tab from './src/Component/tab.js';
+
+// import body from './src/Component/body.js';
+
+
+// const Tab = createBottomTabNavigator();
+
+// const Stack = createStackNavigator();
+
